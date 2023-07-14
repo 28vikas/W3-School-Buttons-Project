a91dd6f4df0dc9bcd29b34a8e9019963b30db0ba
28vikas <63vikas@gmail.com>
2023-07-14 13:35:52 +0530

    read more btn

---
+++ 8.Read More Read less/app.js
@@ -0,0 +1,19 @@
+
+
+function myFunction(){
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("btn1")
+    
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } 
+      else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+
+}
